0ecf32d70fa94c92facaf2bf29a156b24e535462
Your Name <you@example.com>
2020-10-15 09:17:52 +0200

    hello

--- lib/src/data/models/PermissionIdModel.dart
+++ lib/src/data/models/PermissionIdModel.dart
@@ -1,13 +1,24 @@
 
 
 
+import 'dart:convert';
+
+PermissionIdModel permissionIdModelFromJson(String str) => PermissionIdModel.fromJson(json.decode(str));
+
+String permissionIdModelToJson(PermissionIdModel data) => json.encode(data.toJson());
+
 class PermissionIdModel {
       String name;
       String id;
       PermissionIdModel({this.name,this.id});
-      factory PermissionIdModel.fromjson(Map <String,dynamic> jsondata)=>
+      factory PermissionIdModel.fromJson(Map <String,dynamic> jsondata)=>
       PermissionIdModel(
         name: jsondata["name"],
         id: jsondata["id"]
       );
+      
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "Name": name,
+      };
 }
